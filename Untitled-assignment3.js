341b80ecabadc95ec9dc29f1fe9ed4510765d698
shubham <shubham.15561@sscbs.du.ac.in>
2021-01-15 19:42:44 +0530

    second and third assgnment

--- Untitled-assignment3.js
+++ Untitled-assignment3.js
@@ -6,7 +6,13 @@ const obj = {
 }
 keys = ["a", "d"]
     // Question 1
-    //function should delete all the keys from initial Object and return the original object
+
+/**
+ * function should delete all the keys from initial Object
+ * @param object and array of strings
+ * @return the original object
+ */
+
 function deleteKeys(obj, keys) {
     for (const key of keys) {
         delete obj[key]
@@ -18,7 +24,11 @@ console.log(deleteKeys(obj, keys))
 
 
 // Question 2
-///function should delete all the keys from initial Object and return the new object
+/**
+ * function should delete all the keys from initial Object
+ * @param object and array of strings
+ * @return return the new object
+ */
 
 function deleteKeysNew(obj, keys) {
     const newObj = {}
@@ -34,7 +44,12 @@ function deleteKeysNew(obj, keys) {
 console.log(deleteKeysNew(obj, keys))
 
 // Question 3
-// Function to Check if a number is a prime or not
+
+/**
+ * Function to Check if a number is a prime or not
+ * @param integer
+ * @return boolean value
+ */
 function checkPrime(n) {
 
     let i, flag = true;
@@ -51,9 +66,11 @@ function checkPrime(n) {
         return false
 }
 
-// Recursive Fnction for Fibonacci Series
-// Argument passed is an integer
 
+/**
+ * Recursive Function for Fibonacci Series
+ * @param integer
+ */
 function printFibonacci(n) {
     if (n < 2) {
         return n;
@@ -64,29 +81,35 @@ function printFibonacci(n) {
     }
 }
 
-// Helper Function to check that last digit is 1 or 9 and also a prime and print fibonacci series 
-function help(n) {
+
+/**
+ * Helper Function to check that last digit is 1 or 9 and also a prime and print fibonacci series .
+ * @param Integer
+ */
+function helperFunc(n) {
     let last_digit = n % 10
 
-    // console.log((last_digit != 1 || last_digit != 9))
-    if (checkPrime(n) && (last_digit != 1 || last_digit != 9) && last_digit != 3 && last_digit != 7) {
-        console.log("Invalid number")
-    } else {
+    if (checkPrime(n) && (last_digit == 1 || last_digit == 9) && (last_digit != 3 && last_digit != 7)) {
         for (let i = 0; i <= n; i++)
             console.log(printFibonacci(i))
+    } else {
+        console.log("Invalid number")
     }
 
 }
-help(13)
+helperFunc(13)
 
 // QUESTION 4
-// function to generate & print random number between 1-100 till the random number generated 
-//  (end with 9 divisible by 3) or (end with 6 and divisble by 4).
 
+/**
+ * function to generate & print random number between 1-100 till the random number generated 
+ * @param --
+ * @return void
+ */
 function randomGenerator() {
 
     while (true) {
-        let num = Math.floor(Math.random() * 100) + 1; // for generating random no from 1 to 100
+        const num = Math.floor(Math.random() * 100) + 1; // for generating random no from 1 to 100
         const last_digit = num % 10
         if ((last_digit == 9 && num % 3 == 0) || (last_digit == 6 && num % 4 == 0)) {
             console.log(`Condition is true here ${num}`)
--- assignment2/script.js
+++ assignment2/script.js
@@ -3,10 +3,11 @@ let history = []
 function search(ele) {
 
 
-    if (event.key === 'Enter') {
+    if (event.key == 'Enter') {
 
         window.open(`https://www.google.com/search?hl=en&source=hp&ei=BVz5X9SaMOXYz7sPj6SAiAg&q=${ele.value}&oq=${ele.value}&gs_lcp=CgZwc3ktYWIQAzIQCC4QsQMQgwEQyQMQChCTAjICCAAyCAguEMcBEKMCMgcIABCxAxAKMgIILjIICC4QxwEQrwEyAggAMgoIABCxAxCDARAKMgIIADICCC46CwgAELEDEIMBEMkDOggIABCxAxCDAToFCAAQsQM6DgguELEDEIMBEMkDEJMCOggILhCxAxCDAToFCC4QsQM6CwguELEDEMcBEKMCOgsILhCxAxDJAxCTAjoOCC4QsQMQgwEQxwEQowI6CwguELEDEMcBEK8BULoPWPUZYNUbaAFwAHgAgAHLAYgBugiSAQUwLjYuMZgBAKABAaoBB2d3cy13aXqwAQA&sclient=psy-ab&ved=0ahUKEwiUsZ3jqo7uAhVl7HMBHQ8SAIEQ4dUDCAc&uact=5`);
         history.push(ele.value)
+        console.log(ele.value)
 
     }
 
