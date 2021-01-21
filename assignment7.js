a0fd3b7ecf95ddcc6ef59a11a31756960384770c
shubham <shubham.15561@sscbs.du.ac.in>
2021-01-21 14:57:18 +0530

    comments added

--- assignment7.js
+++ assignment7.js
@@ -1,32 +1,45 @@
 const fetch = require("node-fetch");
+
+/**
+ * function that fetches all the country details
+ * @param --
+ * @return -- promise
+ */
+
 const countriesData = function() {
-    console.log(" Getting countries from each letter of english")
-    return fetch('https://raw.githubusercontent.com/shbhm-dev/VSassignment/master/countries.json')
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error(`HTTP error! status: ${res.status}`);
-            } else {
-                console.log("Fetched all the countries (Status Done)")
-                return res.json()
-            }
-        }).catch(err) {
-            console.log(err)
-        }
+        console.log(" Getting countries from each letter of english")
+        return fetch('https://raw.githubusercontent.com/shbhm-dev/VSassignment/master/countries.json')
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error(`HTTP error! status: ${res.status}`);
+                } else {
+                    console.log("Fetched all the countries (Status Done)")
+                    return res.json()
+                }
+            })
+    }
+    /**
+     * function that fetches teh capital of the country
+     * @param -- country (Type : String) for which the caital is to be found
+     * @return -- promise
+     */
 
-}
 const countryCapital = function(country) {
-    return fetch(`https://restcountries.eu/rest/v2/name/${country}`)
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error(`HTTP error! status: ${res.status}`);
-            } else {
-                return res.json()
-            }
-        }).catch(err) {
-            console.log(err)
-        }
+        return fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error(`HTTP error! status: ${res.status}`);
+                } else {
+                    return res.json()
+                }
+            })
 
-}
+    }
+    /**
+     * function that fetches weather capital of the country
+     * @param -- capital (Type : String) for which the weather is to be found
+     * @return -- promise
+     */
 
 const getWeather = function(city) {
 
@@ -41,31 +54,45 @@ const getWeather = function(city) {
         })
 }
 
+/**
+ * Async function that fetches  country  starting from 26 letters of english from the countries.json and if there is no country with a specific alphabet, throw an error which will result in printing to console ‘No country found from alphabet <alphabet>.’ and the program should continue.
+ * @param -- --
+ * @return -- promise
+ */
+
+
 const getCountriesData = async function() {
-    const data = await countriesData()
+        const data = await countriesData()
 
-    const alphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        const alphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-    const countries = Object.keys(data)
+        const countries = Object.keys(data)
 
 
-    for (const alphabet of alphabets) {
-        try {
-            if (countries.some((country) => country[0] == alphabet)) {
-                // console.log(aphabet)
-            } else {
-                throw new Error(`‘No country found from alphabet ${alphabet}`)
+        for (const alphabet of alphabets) {
+            try {
+                if (countries.some((country) => country[0] == alphabet)) {
+                    // console.log(aphabet)
+                } else {
+                    throw new Error(`‘No country found from alphabet ${alphabet}`)
+                }
+            } catch (err) {
+                console.log(err)
             }
-        } catch (err) {
-            console.log(err)
         }
+
+
+
+        getCountryCapital(countries, data)
+
     }
-
-
-
-    getCountryCapital(countries, data)
-
-}
+    /**
+     * Async function that   check whether that city exists in the countries.json for that country or not.
+     *  If it does not exist, throwing an error
+     * If it exists, then  get the weather information for that city
+     * @param -- countries(Array of strings) jsonData(JSON)
+     * @return -- promise
+     */
 const getCountryCapital = async function(countries, data) {
 
     let totalCapitals = 0
